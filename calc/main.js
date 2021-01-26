6eba20c934b49a3484b93a2e1f4803d55fb9dc01
yono-oko <57371137+yono-oko@users.noreply.github.com>
2021-01-26 15:55:11 -0500

    some calc changes

---
+++ calc/main.js
@@ -0,0 +1,26 @@
+//main scripts for calc web page
+document.getElementById("b7").addEventListener("click", buttonPress("7"));
+document.getElementById("b8").addEventListener("click", buttonPress("8"));
+document.getElementById("b9").addEventListener("click", buttonPress("9"));
+document.getElementById("b+").addEventListener("click", buttonPress("+"));
+
+document.getElementById("b4").addEventListener("click", buttonPress("4"));
+document.getElementById("b5").addEventListener("click", buttonPress("5"));
+document.getElementById("b6").addEventListener("click", buttonPress("6"));
+document.getElementById("b-").addEventListener("click", buttonPress("-"));
+
+document.getElementById("b1").addEventListener("click", buttonPress("1"));
+document.getElementById("b2").addEventListener("click", buttonPress("2"));
+document.getElementById("b3").addEventListener("click", buttonPress("3"));
+document.getElementById("bx").addEventListener("click", buttonPress("x"));
+
+document.getElementById("bc").addEventListener("click", buttonPress("c"));
+document.getElementById("b0").addEventListener("click", buttonPress("0"));
+document.getElementById("b.").addEventListener("click", buttonPress("."));
+document.getElementById("b/").addEventListener("click", buttonPress("/"));
+
+var eq = document.getElementById("eq");
+
+function buttonPress(button) {
+  eq.innerHTML = button;
+}
